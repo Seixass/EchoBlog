e584c2078cb6b2a0801d7d256a61e8e0f7a42b2d
seixass <victorseixasmcz@gmail.com>
2024-09-06 07:53:15 -0300

    RF02 concluida

--- backend/src/controllers/postsController.js
+++ backend/src/controllers/postsController.js
@@ -20,17 +20,60 @@ const createSchema = z.object({
 
 export const create = async (req, res) => {
   try {
-    const { titulo, conteudo, autor, imagem } = req.body;
+    const { titulo, conteudo, autor } = createSchema.parse(req.body);
+
+
     const newPost = await Post.create({
       titulo,
       conteudo,
       dataPublicacao: new Date(),
       autor,
-      imagem
+      imagem: req.body.imagem || null, 
     });
+
     res.status(201).json(newPost);
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => ({
+          path: err.path,
+          message: err.message,
+        })),
+      });
+    }
     console.error(error);
     res.status(500).json({ error: "Erro ao criar o post" });
+  }
+};
+
+
+export const listarPostagens = async (req, res) => {
+  try {
+    const itemsPorPagina = parseInt(req.query.limit) || 10; 
+    const paginaAtual = parseInt(req.query.page) || 1; 
+    const offset = (paginaAtual - 1) * itemsPorPagina; 
+
+    const { count: totalPostagens, rows: postagens } = await Post.findAndCountAll({
+      limit: itemsPorPagina,
+      offset: offset,
+    });
+
+    const totalPaginas = Math.ceil(totalPostagens / itemsPorPagina);
+
+    const proximaPagina = paginaAtual < totalPaginas
+      ? `${req.protocol}://${req.get('host')}${req.baseUrl}/postagens?page=${paginaAtual + 1}&limit=${itemsPorPagina}`
+      : null;
+
+    res.status(200).json({
+      totalPostagens,
+      totalPaginas,
+      paginaAtual,
+      itemsPorPagina,
+      proximaPagina,
+      postagens,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao listar postagens" });
   }
 };
--- backend/src/router/postsRoutes.js
+++ backend/src/router/postsRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { create } from "../controllers/postsController.js";
+import { create, listarPostagens } from "../controllers/postsController.js";
 
 const router = Router();
 
 
 router.post("/", create);
+router.get("/", listarPostagens);
 
 export default router;
